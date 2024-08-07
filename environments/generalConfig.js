a740ee96019331e02fa5a3a62994c4a92c364ec4
ACahuiche <alberto.cahuiche@outlook.com>
2024-08-06 22:54:00 -0600

    se agrega la conexion a Atlas

---
+++ environments/generalConfig.js
@@ -0,0 +1,6 @@
+const generalConfig = {
+    mongoLocalConnection: 'mongodb://localhost:27017/drahgonia',
+    mongoConnection: 'mongodb+srv://albertomanuelcahuiche:3wxG9qp66oeDdITq@cahuichecluster.jhtbe3y.mongodb.net/drahgonia?retryWrites=true&w=majority&appName=CahuicheCluster'
+  };
+  
+  module.exports = generalConfig;
