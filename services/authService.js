ae67415b1d008a17c8a01665e76e50e164d223d8
ACahuiche <alberto.cahuiche@outlook.com>
2024-08-07 23:44:25 -0600

    Se mejora la seguridad agregando una API Key

--- services/authService.js
+++ services/authService.js
@@ -1,14 +1,22 @@
 const authDAO = require("../DAO/authDAO");
+const serverAPIKey = require("../config/securityConfig").jwtconfig;
 
 class AuthService {
   async validateUserData(userData) {
-    if (userData.userEmailData.trim() == "" || userData.userEmailData == "undefined" ||
-    userData.passwordData.trim() == "" || userData.passwordData == "undefined") {
-      throw new Error("Some data is empty, validate the username os password");
+    if (typeof userData.userEmailData === "undefined"  || typeof userData.passwordData === "undefined" ||
+      userData.userEmailData.trim() == "" || userData.passwordData.trim() == "") {
+      throw new Error("Algunos campos estan vacios, verifica el correo o la contraseña");
     }
-    else {
-      return await authDAO.loginAuth(userData);
+
+    if(typeof userData.apiKey === 'undefined' || userData.apiKey.trim == ""){
+      throw new Error("No se envio la API Key, contacta al administrador para obtenerla");
     }
+
+    if(userData.apiKey != serverAPIKey.backkey){
+      throw new Error("La API Key es incorrecta");
+    }
+
+    return await authDAO.loginAuth(userData);
   }
 }
 
